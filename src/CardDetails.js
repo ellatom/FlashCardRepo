05406deea2eddcb0f423e23799fd1b492b441db9
ellatom <68844149+ellatom@users.noreply.github.com>
2020-10-04 13:38:06 +0300

    Add files via upload

---
+++ src/CardDetails.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class CardsDetail extends React.Component {  
+
+    onDelete = async (event) => {
+        this.props.onDelete(event, this.props.keyCard);
+    }
+    onEdit = async (event) => {
+        this.props.onEdit(event, this.props.card.id);
+    }   
+    
+
+    render()
+    {
+        if(this.props.card == null) return "no card";
+        return(  
+           <div className="cardView">
+               <div className="question" key={this.props.card.id}><i className="far fa-question-circle"></i>Question:{this.props.card.question}</div>
+               <div className="answer"><i className="fas fa-crown"></i>Answer:{this.props.card.answer}</div>
+               <button className="delete" onClick={this.onDelete}>Delete</button>
+               <button className="edit" onClick={this.onEdit}>Edit</button>
+           </div> 
+        )
+    }
+}
+
+export default CardsDetail;
